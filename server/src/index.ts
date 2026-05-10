8c5c536d553f6f64f1e2acb237e928539ada62fa
Hp <rs739025@gmail.com>
2026-05-10 19:44:38 +0530

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ import dashboardRoutes from './routes/dashboard.routes'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
 
 app.use(cors({
     origin: process.env.CLIENT_URL || 'http://localhost:5173',
